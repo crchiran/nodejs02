7493a717bdc283ce323132f053c3aea46203ccda
crchiran <datatigerbd@gmail.com>
2022-10-31 08:10:34 +0600

    asfasfasfa

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ tar czf nodejs02.tar.gz main.js package.json public LICENSE
 scp nodejs02.tar.gz root@170.187.228.173:/srv/
 rm nodejs02.tar.gz
 
+
 ssh 170.187.228.173 << 'ENDSSH'
 cd /srv
 pm2 stop all

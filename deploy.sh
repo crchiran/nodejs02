a8f0030360064a7be10a159efc02232051a22330
crchiran <datatigerbd@gmail.com>
2022-10-31 08:04:39 +0600

    wqrqrqwrq

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-tar czf easyio.tar.gz main.js package.json public LICENSE
-scp easyio.tar.gz nanogram@nanogram.io:~
-rm easyio.tar.gz
+tar czf nodejs02.tar.gz main.js package.json public LICENSE
+scp nodejs02.tar.gz root@170.187.228.173:/srv/
+rm nodejs02.tar.gz
 
-ssh nanogram@nanogram.io << 'ENDSSH'
+ssh 170.187.228.173 << 'ENDSSH'
+cd /srv
 pm2 stop all
-rm -rf easyio
-mkdir easyio
-tar xf easyio.tar.gz -C easyio
-rm easyio.tar.gz
+rm -rf nodejs02
+mkdir nodejs02
+tar xf nodejs02.tar.gz -C easyio
+rm nodejs02.tar.gz
 cd easyio 
 npm i
 pm2 start all
